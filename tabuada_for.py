0a5bca578af05d374ee90487f18e847e3e9d5b0b
cauacunhaf <cauadacunhaf@gmail.com>
2025-04-26 18:22:02 -0300

    remover tabuada_for.py

--- tabuada_for.py
+++
@@ -1,8 +0,0 @@
-numA = int(input("Digite um número:"))
-numB = int(input("Digite outro número:"))
-
-
-for cont in range (0,11):
-    multi = numA*numB
-    print(f"{numA}x{numB}={multi}")
-    numB+=1
