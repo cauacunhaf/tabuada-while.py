3a4affaa90bddf1c7eb145c181a7ccb647718cc7
cauacunhaf <cauadacunhaf@gmail.com>
2025-04-26 18:00:16 -0300

    commit tabuada while

---
+++ tabuada_while.py
@@ -0,0 +1,14 @@
+
+numA = int(input("Digite um número:"))
+numB = int(input("Digite outro número:"))
+
+
+while numB <= 10:
+    multi = numA*numB
+    print(f"{numA}x{numB}={multi}")
+    numB +=1
+
+
+
+
+
